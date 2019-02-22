ea6c90d8471f41dd3fc0bfd6bcf41222a21e7aa3
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-02-22 13:19:30 -0500

    Successful GET to /api/actions/:id

--- routes/actionRoutes.js
+++ routes/actionRoutes.js
@@ -8,10 +8,12 @@ const db = knex(knexConfig.development);
 
 // POST to /api/actions
 router.post("/", (req, res) => {
-    const { action_name, project_id } = req.body;
+  const { action_name, project_id } = req.body;
 
   if (!action_name || !project_id) {
-    return res.status(400).send("You must submit an action name and project ID.");
+    return res
+      .status(400)
+      .send("You must submit an action name and project ID.");
   }
 
   db("actions")
@@ -27,15 +29,36 @@ router.post("/", (req, res) => {
 
 // GET /api/actions
 router.get("/", (req, res) => {
-    db("actions")
-        .then(actions => {
-            res.status(200).json(actions);
-        }).catch(err => {
-            res.status(500).send("There was an error retrieving the data.")
-        });
-})
+  db("actions")
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(err => {
+      res.status(500).send("There was an error retrieving the data.");
+    });
+});
 
 // GET /api/actions/:id
+
+router.get("/:id", (req, res) => {
+  db("actions")
+    .where("id", req.params.id)
+    .first()
+    .then(action => {
+      if (action) {
+        res.status(200).json(action);
+      } else {
+        res
+          .status(404)
+          .json({
+            error: `An action with ID ${req.params.id} does not exist.`
+          });
+      }
+    })
+    .catch(err => {
+      res.status(500).send("There was an error retrieving the data.");
+    });
+});
 
 // PUT /api/actions/:id
 
