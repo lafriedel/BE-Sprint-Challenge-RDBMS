d46d68b2239a79f1daa9fcf687d9917c85511b7c
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-02-22 13:15:34 -0500

    Successful PUT to /api/projects/:id

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -8,53 +8,83 @@ const db = knex(knexConfig.development);
 
 // POST to /api/projects
 router.post("/", (req, res) => {
-    const { name } = req.body;
-  
-    if (!name) {
-      return res.status(400).send("You must submit a project name.");
-    }
-  
-    db("projects")
-      .insert(req.body)
-      .then(projid => {
-        const [id] = projid;
-        res.status(201).json({ id: id });
-      })
-      .catch(err => {
-        res.status(500).send("There was an error adding the project.");
-      });
-  });
+  const { name } = req.body;
 
-  // GET to /api/projects
-  router.get("/", (req, res) => {
-      db("projects")
-        .then(projects => {
-            res.status(200).json(projects);
-        }).catch(err => {
-            res.status(500).send("There was an error retrieving the data.")
-        })
-  })
+  if (!name) {
+    return res.status(400).send("You must submit a project name.");
+  }
 
-  // GET to /api/projects/id
+  db("projects")
+    .insert(req.body)
+    .then(projid => {
+      const [id] = projid;
+      res.status(201).json({ id: id });
+    })
+    .catch(err => {
+      res.status(500).send("There was an error adding the project.");
+    });
+});
+
+// GET to /api/projects
+router.get("/", (req, res) => {
+  db("projects")
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err => {
+      res.status(500).send("There was an error retrieving the data.");
+    });
+});
+
+// GET to /api/projects/id
 router.get("/:id", (req, res) => {
-    db("projects")
-      .where("id", req.params.id)
-      .first()
-      .then(projectRes => {
-        const { id, name, description, completed } = projectRes;
-  
-        db("actions")
-          .select("id", "action_name as description", "notes", "completed")
-          .where("project_id", projectRes.id)
-          .then(actionsRes => {
-            res
-              .status(200)
-              .json({ id, name, description, completed, actions: actionsRes });
+  db("projects")
+    .where("id", req.params.id)
+    .first()
+    .then(projectRes => {
+      const { id, name, description, completed } = projectRes;
+
+      db("actions")
+        .select("id", "action_name as description", "notes", "completed")
+        .where("project_id", projectRes.id)
+        .then(actionsRes => {
+          res
+            .status(200)
+            .json({ id, name, description, completed, actions: actionsRes });
+        });
+    })
+    .catch(err => {
+      res.status(500).send("There was an error retrieving the data.");
+    });
+});
+
+// PUT to /api/projects/:id
+
+router.put("/:id", (req, res) => {
+  db("projects")
+    .where("id", req.params.id)
+    .update(req.body)
+    .then(count => {
+      if (count === 0) {
+        res
+          .status(404)
+          .json({
+            error: `A project with ID ${req.params.id} does not exist.`
           });
-      })
-      .catch(err => {
-        res.status(500).send("There was an error retrieving the data.");
-      });
-  });
+      } else {
+        db("projects")
+          .where("id", req.params.id)
+          .first()
+          .then(project => {
+            res.status(200).json(project);
+          });
+      }
+    })
+    .catch(err => {
+      res.status(500).send("There was an error updating the project.");
+    });
+});
+
+// DELETE to /api/projects/:id
 
 module.exports = router;
